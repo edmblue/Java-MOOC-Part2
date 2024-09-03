f25aa1687209776105d4989ebc13967eadf565ef
Raquel Betancourt <114664493+edmblue@users.noreply.github.com>
2024-09-03 11:19:49 -0300

    Add files via upload

---
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class RepeatingBreakingAndRemembering {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0;
+        int count = 0;
+        int evenCount = 0;
+        int oddCount = 0;
+        
+        System.out.println("Give numbers:");
+        
+        while (true) {
+            
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == -1) {
+                System.out.println("Thx! Bye!");
+                break;
+            }
+            
+            sum += number;
+            count++;
+
+            if (number % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+
+        }
+
+        double average = (double) sum / count;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + count);
+        System.out.println("Average: " + average);
+        System.out.println("Even: " + evenCount);
+        System.out.println("Odd: " + oddCount);
+
+    }
+}
