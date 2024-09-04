8cd7986b3630bb07d82ede1e286315b497808435
Raquel Betancourt <114664493+edmblue@users.noreply.github.com>
2024-09-04 07:59:43 -0300

    Add files via upload

---
+++ part02-Part02_25.Division/src/main/java/Division.java
@@ -0,0 +1,15 @@
+
+
+public class Division {
+
+    public static void main(String[] args) {
+         division(3, 5);
+    }
+
+    public static void division(int num, int den) {
+        double div = (double) num / den;
+        System.out.println(div);
+    }
+
+    
+}
