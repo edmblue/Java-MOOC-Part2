b1e3ed92d733eb345a29a03ed7bf6b2e5d8ecde7
Raquel Betancourt <114664493+edmblue@users.noreply.github.com>
2024-09-06 22:08:45 -0300

    Add files via upload

---
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -0,0 +1,32 @@
+
+public class StarSign {
+
+    public static void main(String[] args) {
+        printTriangle(4);
+    }
+
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            printStars(size);
+        }
+    }
+
+    public static void printRectangle(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            printStars(width);
+        }
+    }
+
+    public static void printTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            printStars(i);
+        }
+    }
+}
